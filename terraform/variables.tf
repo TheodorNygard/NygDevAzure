92e0e0efd534e637db56695c6a3cc0756d7018ef
Theodor Nygård <49783243+TheodorNygard@users.noreply.github.com>
2026-03-01 09:59:24 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,7 +25,7 @@ variable "vm_name" {
 variable "vm_size" {
   description = "Size of the virtual machine"
   type        = string
-  default     = "Standard_D4as_v5"
+  default     = "Standard_D4als_v6"
 }
 
 variable "admin_username" {
